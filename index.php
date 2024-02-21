bf5cd450e59236f7cecb24a53dccc0201d08b185
PETCU MIHNEA <80165616+M1HNE41@users.noreply.github.com>
2024-02-21 22:09:10 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Website</title>
+</head>
+<body>
+    <h1>Welcome to my website!</h1>
+    <a href="page.php" class="button">Go to Page</a>
+</body>
+</html>
